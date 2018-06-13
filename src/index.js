9383897f74de6c3d0fa6764c6c7bda89e1cdbca4
dcuddhy <dcuddhy@gmail.com>
2018-06-12 21:41:36 -0600

    react hello world

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+
+
+    // Or get do it all in one go.
+    ReactDOM.render(
+      <div>Hello, world!</div>,
+      document.getElementById('root')
+    );
