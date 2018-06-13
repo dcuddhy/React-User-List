4b3f4581af48973306d54a5456ef333a17819360
dcuddhy <dcuddhy@gmail.com>
2018-06-12 22:35:53 -0600

    minor whitespace fix on index.js

--- src/index.js
+++ src/index.js
@@ -8,15 +8,14 @@ import './index.css';
         format: 'json'
     })
     .done(function( data ) {
-
-    $.each( data.data, function( i, d ) {
-        $( '#users-table' ).append(
-            '<tr class="user-list-item">' +
-            '   <td class="user-name">' + JSON.stringify(d['full_name']) + '</td>' +
-            '   <td class="user-email">' + JSON.stringify(d['email']) + '</td>' +
-            '   <td class="user-view">' + '[VIEW BUTTON]' + '</td>' +
-            '   <td class="user-survey-date">' + JSON.stringify(d['survey_date']) + '</td>' +
-            ' </tr>'
-        );
-    });
+        $.each( data.data, function( i, d ) {
+            $( '#users-table' ).append(
+                '<tr class="user-list-item">' +
+                '   <td class="user-name">' + JSON.stringify(d['full_name']) + '</td>' +
+                '   <td class="user-email">' + JSON.stringify(d['email']) + '</td>' +
+                '   <td class="user-view">' + '[VIEW BUTTON]' + '</td>' +
+                '   <td class="user-survey-date">' + JSON.stringify(d['survey_date']) + '</td>' +
+                ' </tr>'
+            );
+        });
     });
