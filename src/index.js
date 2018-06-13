964f7a8cf3892bb47cc1dac6f679e5a41749db62
dcuddhy <dcuddhy@gmail.com>
2018-06-12 21:47:35 -0600

    added jquery to get json easily

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
 import './index.css';
 
 
-
-
-    // Or get do it all in one go.
     ReactDOM.render(
       <div>Hello, world!</div>,
       document.getElementById('root')
     );
+
+
+    var userList = 'http://localhost:3000/api/users';
+    $.getJSON( userList, {
+        format: 'json'
+    })
+    .done(function( data ) {
+      $.each( data.data, function( i, d ) {
+        $( '#users' ).append('<div>' + JSON.stringify(d) + ' </div>');
+      });
+    });
