a7cce7c25dc8239bd006e5bca1b0fa716874d5c9
dcuddhy <dcuddhy@gmail.com>
2018-06-17 20:24:12 -0600

    Added errorBoundary component.  Not really doing anything with it yet.

---
+++ src/components/errorBoundary.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+// Error Handling docs
+// https://reactjs.org/blog/2017/07/26/error-handling-in-react-16.html
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
