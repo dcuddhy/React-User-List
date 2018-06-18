b1f34c179b4e8458daea123dfa21dd050c0365d3
dcuddhy <dcuddhy@gmail.com>
2018-06-18 07:37:00 -0600

    Removed unwanted comment from user info header

--- src/components/userInfoHeader.js
+++ src/components/userInfoHeader.js
@@ -4,10 +4,6 @@ import '../components/userInfoHeader.css';
 
 class UserInfoHeader extends React.Component {
     render() {
-        // TODO Logo.svg uses currentColor to fill circle around logo, but it doesn't
-        // work when SVGs are rendered this way in an img tag.  A quick fix is to create
-        // variations of the logo to hardcode the color we need.  This needs to be refactored
-        // to use currentColor as intended.
         return (
             <div className="user-info-header-container">
                 <div className="user-info-header">
