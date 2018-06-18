2aee80d852909072bc21d327f905debfd58cbc90
dcuddhy <dcuddhy@gmail.com>
2018-06-17 22:11:40 -0600

    Implemented errorBoundary with styled error

--- src/components/errorBoundary.js
+++ src/components/errorBoundary.js
@@ -1,21 +1,36 @@
 import React from 'react';
+import './errorBoundary.css';
+import UserListHeader from '../components/userListHeader';
+
 
 
 // Error Handling docs
 // https://reactjs.org/blog/2017/07/26/error-handling-in-react-16.html
 class ErrorBoundary extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { hasError: false };
-  }
+    state = {
+        hasError: false,
+        error: null,
+        errorInfo: null
+    };
 
-  componentDidCatch(error, info) {
-    this.setState({ hasError: true });
-  }
+    componentDidCatch(error, errorInfo) {
+        this.setState({
+            hasError: true,
+            error,
+            errorInfo
+        });
+    }
 
   render() {
     if (this.state.hasError) {
-      return <h1>Something went wrong.</h1>;
+        return (
+            <div>
+                < UserListHeader />
+                <div className="error-boundary-error">
+                    <h1>Something went wrong.</h1>
+                </div>
+            </div>
+        )
     }
     return this.props.children;
   }
